1426145d24b63348e9aa6428f975ea1b9d403314
Cesaro95 <juliomv1495@gmail.com>
2023-04-20 02:19:26 -0400

    mi primer commit

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+let links = document.querySelectorAll(".close");
+links.forEach(function (link) {
+    link.addEventListener("click", function (ev) {
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+
+        //Quitar clases
+        content.classList.remove("animate__animated");
+        content.classList.remove("animate__fadeInDown");
+        //Añadir clases
+        content.classList.add("animate__animated");
+        content.classList.add("animate__fadeOutUp");
+
+        setTimeout(function () {
+            location.href = "/";
+        }, 600);
+        return false;
+    });
+});
+
